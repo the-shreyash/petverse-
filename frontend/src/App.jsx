9f50e92183039b5f8ced07f3ec9e1e1f31664ceb
shreyash Yadav <shreyashy49@gmail.com>
2026-07-03 16:50:00 +0530

    1. generate the basic frontend design

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Container from "./componenets/container/Container";
+import Button from "./componenets/Button/index.js";
+import Navbar from "./componenets/Navbar/Navbar.jsx";
+function App() {
+  return (
+    <Container>
+      <Navbar/>
+      <main className="min-h-screen bg-slate-50 pt-24">
+        {/*hero section*/}
+      </main>
+      
+    </Container>
+    
+  );
+}
+
+export default App;
---
+++ frontend/src/componenets/Navbar/NavActions.jsx
@@ -0,0 +1,17 @@
+import Button from "../Button";
+
+const NavActions = () => {
+  return (
+    <div className="hidden items-center gap-4 lg:flex">
+      <button className="font-medium text-gray-700 transition hover:text-emerald-600">
+        Sign In
+      </button>
+
+      <Button>
+        Get Started
+      </Button>
+    </div>
+  );
+};
+
+export default NavActions;
