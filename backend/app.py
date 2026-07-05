5c1c4dd614e3dfe4d03e5d50cd83d2736eecc9fd
2005shalini <shalinirichhariya59@gmail.com>
2026-07-05 13:48:49 +0530

    Added signup API and MySQL insert

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+from config import db
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "PetVerse Backend Running Successfully!"
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    cursor = db.cursor()
+   
+    data = request.get_json()
+
+    full_name = data["full_name"]
+    email = data["email"]
+    phone = data["phone"]
+    password = data["password"]
+    query = """
+    INSERT INTO users (full_name, email, phone, password)
+    VALUES (%s, %s, %s, %s)
+    """
+
+    cursor.execute(query, (full_name, email, phone, password))
+    db.commit()
+
+    return "User Registered Successfully!" 
 
 if __name__ == "__main__":
     app.run(debug=True)
